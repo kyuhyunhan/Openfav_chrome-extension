a9e0fc1488b52810b5623ba2488f073e006c6b4a
kyuhyun <kyuhyunhaan@gmail.com>
2020-10-30 21:45:47 +0900

    limit options up to only 5

--- popup.js
+++ popup.js
@@ -1,17 +1,33 @@
 let addFavBtn = document.getElementById('addFavBtn');
 let favs = document.getElementById('favs');
-/*
-addFavBtn.onclick = function() {
-	let instance = document.getElementsByClassName('fav-template-instance')[0];
-	let clnInstance = instance.cloneNode(true);
-	clnInstance.style.display = "inline";
-	favs.appendChild(clnInstance);
-}
-*/
 
+
+/*
+추가 기능 함수. 정상 작동. 추후 기능 추가 시 활용
 addFavBtn.onclick = function() {
 	var favTemplate = document.querySelector('#fav-template')
 	var tbody = document.querySelector('tbody')
 	var clone = document.importNode(favTemplate.content, true)
 	tbody.appendChild(clone)
 }
+*/
+
+
+/*
+삭제 기능 개발 중, CSP로 인해 onclick()이 작동 하지 않는 관계로 함수 사용 불가
+function remove(e) {
+	e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+}
+*/
+
+
+/*
+삭제 기능 개발 중(2), 추후 수정해서 기능 추가
+document.addEventListener('DOMContentLoaded', function() {
+	const rmvBtns = document.getElementsByClassName('fav-remove-btn')
+	console.log(rmvBtns)
+	rmvBtns.prototype.forEach((btn) => {
+			console.log(btn)
+		})
+	})
+*/
