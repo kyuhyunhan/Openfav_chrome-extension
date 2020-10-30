eb818c0a08b39f1b5dccc3763be82d2f7174cf15
kyuhyun <kyuhyunhaan@gmail.com>
2020-10-30 19:30:24 +0900

    added template-cloning function

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
 let addFavBtn = document.getElementById('addFavBtn');
 let favs = document.getElementById('favs');
-
+/*
 addFavBtn.onclick = function() {
 	let instance = document.getElementsByClassName('fav-template-instance')[0];
 	let clnInstance = instance.cloneNode(true);
 	clnInstance.style.display = "inline";
 	favs.appendChild(clnInstance);
 }
+*/
 
-// Vimium을 다시 보니, <template> 태그를 활용해서 클론을 한거 같다. 그걸로 다시 해봐야겠음
+addFavBtn.onclick = function() {
+	var favTemplate = document.querySelector('#fav-template')
+	var tbody = document.querySelector('tbody')
+	var clone = document.importNode(favTemplate.content, true)
+	tbody.appendChild(clone)
+}
